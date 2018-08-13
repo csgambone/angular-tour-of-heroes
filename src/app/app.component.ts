91cba0018a6d095b9514ed329105db954b4f319c
Ciro <csgambone@gmail.com>
2018-08-12 20:56:49 -0400

    laptop commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'abcdefghij1234567890';
+  title = 'abcdefghij1234567890laptop';
 }
